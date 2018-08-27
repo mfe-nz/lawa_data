0cd7d1d5b53f0471a875dbd9e51236d17d6c6eda
ignatiusm <ignatius.menzies@gmail.com>
2018-08-27 12:42:03 +1200

    geom segment graph for reporting

--- src/reporting.R
+++ src/reporting.R
@@ -13,3 +13,16 @@ lapply(object_list$Key, download_data)
 
 data_files <- list.files("data/")
 
+# variable “day” that is in as.Date type)
+
+df <- df[, .(max_day = max(day), min_day = min(day)), by =site]
+df[, duration := as.numeric(max_day - min_day)]
+
+dff_plot <- ggplot() +
+    geom_segment(data = df, aes(x = min_day, y = site, xend = max_day, yend = site, col= site), size = 2)+
+    scale_x_date( breaks=pretty_breaks()) + 
+    xlab('Time') + 
+    theme(axis.text.x = element_text(angle=90, vjust = 0.5),
+          legend.position = NULL)
+
+
