ce2963eee0db0accf6b9a4f6b77835242ecf35fe
ignatiusm <ignatius.menzies@gmail.com>
2018-09-07 01:03:38 +0000

    tidying for eyeballing - now uses Reduce to find rows where all cols are NA

--- src/tidying.R
+++ src/tidying.R
@@ -19,28 +19,40 @@ for (i in 1:nrow(object_list)){
     if(nrow(df) < 1){
         cat("no data for", object_list[i,Key],"\n\n")
     } else {
-        sub_cols <- c("Time",
-                      "Site",
-                      parameter_list[region == gsub("_.*", "", object_list[i,Key]) &
-                                       !is.na(`For ER`), Measurement_w_units])
+        sub_cols <- parameter_list[region == gsub("_.*", "", object_list[i,Key]) &
+                                       !is.na(`For ER`), Measurement_w_units]
         
-        df <- df[,sub_cols, with = FALSE]
-        df <- unique(df)
-        df[, region := gsub("_.*", "", object_list[i,Key])]
+        cols <- c("Time",
+                      "Site", sub_cols)
         
-        write.csv(x = df,
-                  file = paste0(gsub("_.*", "", object_list[i,Key], "_tidy.csv")),
+        
+        df <- df[,cols, with = FALSE]
+        
+        df <- df[!Reduce(`&`, lapply(df[,sub_cols, with = FALSE], is.na))]
+        
+        if(nrow(df) < 1){
+            cat("No data for", object_list[i,Key],"\n\n")
+            
+            df <- NULL
+        } else {
+            df[, region := gsub("_.*", "", object_list[i,Key])]
+            
+            write.csv(x = df,
+                  file = paste0(gsub("_.*", "", object_list[i,Key]), "_tidy.csv"),
                   row.names = FALSE)
-        
-        put_object(file = paste0(gsub("_.*", "", object_list[i,Key], "_tidy.csv")),
+            
+            put_object(file = paste0(gsub("_.*", "", object_list[i,Key]), "_tidy.csv"),
+                   object = paste0(gsub("_.*", "", object_list[i,Key]), "_tidy.csv"),
                    bucket = "lawa.eyeball")
-        
-        df <- NULL
-        
-        file.remove(paste0(gsub("_.*", "", object_list[i,Key], "_tidy.csv")))
-                    
-        cat(object_list[i,Key], "summarised \n\n")
+            
+            df <- NULL
+            
+            file.remove(paste0(gsub("_.*", "", object_list[i,Key]), "_tidy.csv"))
+            
+            cat(object_list[i,Key], "summarised \n\n")
+            }
     }
 }
+    
 
 
